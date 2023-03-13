bbbe857199b104f19f519cfb9905d2f6ec2786f1
Damiot Johan <damiot_j@etna-alternance.net>
2023-03-13 18:33:35 +0100

    test Zoom fonct V2 fix 0.5

--- pages/fonct/index.js
+++ pages/fonct/index.js
@@ -22,35 +22,6 @@ const GenerateComponent = (props) => {
     },
   };
 
-  const [zoom, setZoom] = useState(1);
-  const [videoStream, setVideoStream] = useState(null);
-
-  const handleZoomIn = () => {
-    setZoom((prevZoom) => prevZoom + 0.1);
-  };
-
-  const handleZoomOut = () => {
-    setZoom((prevZoom) => prevZoom - 0.1);
-  };
-
-  const handleScan = (data) => {
-    console.log(data);
-  };
-
-  const handleError = (err) => {
-    console.error(err);
-  };
-
-  const openCamera = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: {
-        facingMode: 'environment',
-        zoom: { ideal: zoom },
-      },
-    });
-    setVideoStream(stream);
-  };
-
   adapter.browserDetails.browser;
   adapter.browserDetails.version;
 
@@ -58,26 +29,31 @@ const GenerateComponent = (props) => {
     <>
       <div className='container'>
         <QrReader
-          delay={300}
-          onError={handleError}
-          onScan={handleScan}
-          style={{ width: '100%' }}
+          className={className}
+          constraints={{
+            minAspectRatio: 1.333,
+            minFrameRate: 30,
+            height: 720,
+            width: 1280,
+            resizeMode: 'crop-and-scale',
+            aspectRatio: { min: 1, max: 2 },
+            facingMode: 'environment',
+          }}
+          onResult={(result, error) => {
+            if (!!result) {
+              setData(result?.text);
+              setIsOpen(true);
+              console.log(result);
+            }
+
+            // if (!!error) {
+            //   console.info(error);
+            // }
+          }}
         />
-        <div>
-          <button onClick={handleZoomIn} className='link'>
-            Zoom +
-          </button>
-          <button onClick={handleZoomOut} className='link'>
-            Zoom -
-          </button>
-          <button onClick={openCamera} className='link'>
-            Ouvrir la caméra
-          </button>
-        </div>
-        <video
-          ref={(video) => videoStream && (video.srcObject = videoStream)}
-          style={{ display: 'none' }}
-        />
+        <button className='link' onClick="#">
+          zoom
+        </button>
         <a href='/' className='link'>
           return Home
         </a>
