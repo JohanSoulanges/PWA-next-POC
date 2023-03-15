6d66ed8a078e0cc31d216a9276a5dce710bab3a8
Damiot Johan <damiot_j@etna-alternance.net>
2023-03-15 11:48:37 +0100

    Fix CSS

--- pages/qrCode/index.js
+++ pages/qrCode/index.js
@@ -16,12 +16,14 @@ const GenerateComponent = (props) => {
     content: {
       top: '50%',
       left: '50%',
-      with: '99vw',
-      right: 'auto',
       bottom: 'auto',
       marginRight: '-50%',
       transform: 'translate(-50%, -50%)',
     },
+    style: {
+      with: '99vw',
+      right: 'auto',
+    }
   };
 
   // Decrypt
